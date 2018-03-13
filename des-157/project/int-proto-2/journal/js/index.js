31c9d8d5969809d600dbcb51f080dbe15d62f25d
Adeline Soekawan <35323922+adellevana@users.noreply.github.com>
2018-03-13 14:04:40 -0700

    Add files via upload

---
+++ des-157/project/int-proto-2/journal/js/index.js
@@ -0,0 +1,25 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+let container = document.querySelector('.container');
+container.appendChild(p);
+console.log(container);
+
+recognition.addEventListener('result', function(e) {
+  const speechTotext = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('')
+			
+  p.textContent = speechTotext;
+
+  if (e.results[0].isFinal) {
+		p = document.createElement('p');
+	  container.appendChild(p);
+	}
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
